6dfba988bf2e7076e2c28c50d59425d0dc585c06
Cláudio Fernando Leitão <claudio@pop-os.localdomain>
2022-09-05 23:43:18 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,6 +1,5 @@
-import logo from '../../assets/img/logo.svg'
-
-import './styles.css'
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
 
 function Header() {
     return(
@@ -14,7 +13,7 @@ function Header() {
                 </p>
             </div>
         </header>
-    )
+    );
   }
   
-  export default Header
+  export default Header;
