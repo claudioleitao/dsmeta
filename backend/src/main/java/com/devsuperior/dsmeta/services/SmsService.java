b31ae7ef34d8654588564aed72fa16935a2b28fb
Cláudio Fernando Leitão <claudio@pop-os.localdomain>
2022-09-08 16:51:39 -0300

    correção smsService

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -36,9 +36,16 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
-
+/*		String msg = "O vendedor "+ sale.getSellerName() + " foi destaque em " + date
+				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
+*/
+		String amount = new DecimalFormat("###0.00").format(sale.getAmount());
+		
+		System.out.println("Amount = " + amount);
+		
+		String msg = "O vendedor "+ sale.getSellerName() + " foi destaque em " + date
+				+ " com um total de R$ " + amount;
+		
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
